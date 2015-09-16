fd2b802b4731e26d5ef8fe8d5e6b3135be030f29
Ebowe Blessing <blessing.ebowe@andela.com>
2015-09-16 08:38:51 +0100

    Style edits

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -14,10 +14,10 @@ Rails.application.configure do
 
   # Configure static file server for tests with Cache-Control for performance.
   config.serve_static_files   = true
-  config.static_cache_control = 'public, max-age=3600'
+  config.static_cache_control = "public, max-age=3600"
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
 
   # Raise exceptions instead of rendering exception templates.
