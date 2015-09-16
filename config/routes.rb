8982e3a42ac34d45f5e94ea92a8e56857e4c3ff3
Ebowe Blessing <blessing.ebowe@andela.com>
2015-09-16 08:06:12 +0100

    Style editing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
+  # The priority is based upon order of 
+  # creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  #my own root
-  root 'application#goodbye'
+  # my own root
+  root "application#goodbye"
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
@@ -12,7 +13,7 @@ Rails.application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
+  # Example resource route (maps HTTP verbs to controller actions automatically)
   #   resources :products
 
   # Example resource route with options:
